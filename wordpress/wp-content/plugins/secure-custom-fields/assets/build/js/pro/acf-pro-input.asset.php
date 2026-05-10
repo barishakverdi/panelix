522c0d0f2597412d75d149737f8a95d386653224
Barış Hakverdi <info@barishakverdi.com>
2026-05-10 11:28:55 +0300

    frontend

--- wordpress/wp-content/plugins/secure-custom-fields/assets/build/js/pro/acf-pro-input.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '262ef34e5c98544e32e8');
--- wordpress/wp-content/plugins/secure-custom-fields/assets/build/js/pro/acf-pro-input.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '8b148a0cd0ccbb3d8dab');
